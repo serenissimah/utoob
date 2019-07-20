5c31caa1e7bca613cc2f9946d74180d1964c1697
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-07-20 10:23:33 -0600

    models controllers and routes are set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :videos, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
---
+++ db/migrate/20190720160701_create_videos.rb
@@ -0,0 +1,13 @@
+class CreateVideos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.string :duration
+      t.string :genre
+      t.text :description
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
