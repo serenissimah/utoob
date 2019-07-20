fd72d161821a94f3a96622eb245465a9c9030031
Serena <serenissimah@gmail.com>
2019-07-20 14:43:08 -0600

    pulled from master

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Header, } from 'semantic-ui-react';
-import DisplayVideo from './Videos/ProfileVideos';
+import DisplayVideo from './Videos/DisplayVideo';
+// import Comments from '../components/comments/Comments'
+import Axios from 'axios';
+import ProfileVideos from './Videos/ProfileVideos';
 
-const Home = () => (
+
+
+const Home = () => ( 
   <>
-    <Header as="h3" textAlign="center">Devise App</Header>
-    <DisplayVideo />
+    {/* <Header as="h3" textAlign="center">UToob</Header> */}
+    {/* {VideoTitle} */}
+    {/* <Comments */}
+    <ProfileVideos />
+    {/* // video_id={video.id} */}
+   
     </>
 )
 
---
+++ client/src/components/Videos/DisplayVideo.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Header, Image } from 'semantic-ui-react';
+
+const Video = (props) => {
+    const [ video, setVideo ] = useState([]);
+};
+
+const DisplayVideo = () => (
+ <>
+ {/* <Image src={Utoob} size='small'/> */}
+
+    <Header as='h3' textAlign='center'>
+   Video Title
+        {/* {VideoTitle} */}
+    </Header>
+ </>
+);
+
+export default DisplayVideo;
