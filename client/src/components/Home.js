8afcef98229cb2e31fba8b22ec3d8ba385fe5b49
Michael Brown <Vurclash@gmail.com>
2019-07-20 10:54:49 -0600

    Added Display Video Framework

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Header, } from 'semantic-ui-react';
+import DisplayVideo from './Videos/DisplayVideo';
 
 const Home = () => (
+  <>
     <Header as="h3" textAlign="center">Devise App</Header>
+    <DisplayVideo />
+    </>
 )
 
 export default Home;
