53c9bafd8b5d85d93da70edfa36a8ce09636fb67
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-07-20 15:12:20 -0600

    things be messin up

--- client/src/components/Videos/ProfileVideos.js
+++ client/src/components/Videos/ProfileVideos.js
@@ -3,25 +3,20 @@ import { Header, Card, Divider, Image } from 'semantic-ui-react'
 import { Link, } from 'react-router-dom'
 import axios from 'axios'
 import DisplayVideo from './ProfileVideos'
-
 class ProfileVideos extends React.Component {
   state = { videos: [], }
-
   componentDidMount() {
     axios.get('/api/videos')
       .then(res => this.setState({ videos: res.data, }))
   };
-
-
-
   render() {
     return (
       <>
         <Header>Render Videos</Header>
         <Card.Group itemsPerRow={4}>
           {this.state.videos.map(videos =>
-            <Link to="/DisplayVideos">
-              <Card key={videos.id} >
+            <Link to={`/videos/${videos.id}`}>
+              <Card key={videos.id}>
                 <Image src={videos.trailer} />
                 <Card.Content>
                   <Divider />
@@ -35,5 +30,4 @@ class ProfileVideos extends React.Component {
     )
   }
 }
-
 export default ProfileVideos
